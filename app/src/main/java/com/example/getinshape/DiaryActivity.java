498512f1df6e43b9ffffa49263f66695c3a2b363
Rafael Bastos <rafabastos@gmail.com>
2023-07-13 20:26:56 -0700

    RecyclerView working, showing each food added as cards

--- app/src/main/java/com/example/getinshape/DiaryActivity.java
+++ app/src/main/java/com/example/getinshape/DiaryActivity.java
@@ -2,11 +2,14 @@ package com.example.getinshape;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.Adapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,21 +19,39 @@ import java.util.ArrayList;
 
 public class DiaryActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    ArrayList<String> food_name, serving_size, calories, timestamp;
+    DBHelper db;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.Adapter adapter;
+
+//    Adapter adapter;
+
     private TextView diaryFood, diaryServing, diaryCalories, diaryDate;
 
-    DBHelper db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
 
-        diaryFood = findViewById(R.id.diary_food);
-        diaryServing = findViewById(R.id.diary_serving);
-        diaryCalories = findViewById(R.id.diary_calories);
-        diaryDate = findViewById(R.id.diary_date);
+//        diaryFood = findViewById(R.id.diary_food);
+//        diaryServing = findViewById(R.id.diary_serving);
+//        diaryCalories = findViewById(R.id.diary_calories);
+//        diaryDate = findViewById(R.id.diary_date);
 
         db = new DBHelper(this);
+        food_name = new ArrayList<>();
+        serving_size = new ArrayList<>();
+        calories = new ArrayList<>();
+        timestamp = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.recycler_view);
+        adapter = new RecyclerAdapter(this, food_name, serving_size, calories, timestamp);
+        recyclerView.setAdapter(adapter);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
 
         loadData();
     }
@@ -43,20 +64,27 @@ public class DiaryActivity extends AppCompatActivity {
             Toast.makeText(DiaryActivity.this, "No entry found", Toast.LENGTH_SHORT).show();
             return;
         }
-
-        System.out.println("*************************************");
-        System.out.println(result);
-        System.out.println("*************************************");
-
-        StringBuffer buffer = new StringBuffer();
-        while(result.moveToNext()) {
-            buffer.append("localDateTimeNow : " + result.getString(0)+"\n");
-            buffer.append("food : " + result.getString(1)+"\n");
-            buffer.append("serving : " + result.getString(2)+"\n");
-            buffer.append("calories : " + result.getString(3)+"\n\n");
+        else {
+            while(result.moveToNext()) {
+                timestamp.add(result.getString(0));
+                food_name.add(result.getString(1));
+                serving_size.add(result.getString(2));
+                calories.add(result.getString(3));
+            }
         }
 
-        diaryFood.setText(buffer.toString());
+
+
+
+//        StringBuffer buffer = new StringBuffer();
+//        while(result.moveToNext()) {
+//            buffer.append("localDateTimeNow : " + result.getString(0)+"\n");
+//            buffer.append("food : " + result.getString(1)+"\n");
+//            buffer.append("serving : " + result.getString(2)+"\n");
+//            buffer.append("calories : " + result.getString(3)+"\n\n");
+//        }
+//
+//        diaryFood.setText(buffer.toString());
 
 //        AlertDialog.Builder builder = new AlertDialog.Builder(DiaryActivity.this);
 //        builder.setCancelable(true);
---
+++ app/src/main/java/com/example/getinshape/RecyclerAdapter.java
@@ -0,0 +1,67 @@
+package com.example.getinshape;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    private Context context;
+    private ArrayList food_id, serving_id, calories_id, timestamp_id;
+
+    public RecyclerAdapter(Context context, ArrayList food_id, ArrayList serving_id, ArrayList calories_id, ArrayList timestamp_id) {
+        this.context = context;
+        this.food_id = food_id;
+        this.serving_id = serving_id;
+        this.calories_id = calories_id;
+        this.timestamp_id = timestamp_id;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card, parent, false);
+        ViewHolder viewHolder = new ViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        holder.food_id.setText(String.valueOf(food_id.get(position)));
+        holder.serving_id.setText(String.valueOf(serving_id.get(position)));
+        holder.calories_id.setText(String.valueOf(calories_id.get(position)));
+        holder.timestamp_id.setText(String.valueOf(timestamp_id.get(position)));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return food_id.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView food_id, serving_id, calories_id, timestamp_id;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            food_id = itemView.findViewById(R.id.food_name);
+            serving_id = itemView.findViewById(R.id.food_serving);
+            calories_id = itemView.findViewById(R.id.food_calories);
+            timestamp_id = itemView.findViewById(R.id.food_timestamp);
+        }
+    }
+
+
+}
